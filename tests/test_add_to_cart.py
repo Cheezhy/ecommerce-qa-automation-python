63ffacbef73437493aded08076e90fdda272f356
Cheezhy <olebuchizitere@gmail.com>
2025-05-19 21:46:20 +0100

    Fix: handle ad iframe in add to cart test using JS click fallback

--- tests/test_add_to_cart.py
+++ tests/test_add_to_cart.py
@@ -1,12 +1,23 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementClickInterceptedException
 
 def test_add_to_cart(driver):
+    driver.set_window_size(1920, 1080)
     driver.get("https://automationexercise.com")
     wait = WebDriverWait(driver, 20)
 
-    wait.until(EC.element_to_be_clickable((By.XPATH, "(//a[contains(text(),'Add to cart')])[1]"))).click()
-    wait.until(EC.element_to_be_clickable((By.XPATH, "//u[contains(text(),'View Cart')]"))).click()
+    add_button = wait.until(
+        EC.presence_of_element_located((By.XPATH, "(//a[contains(text(),'Add to cart')])[1]"))
+    )
+    try:
+        wait.until(EC.element_to_be_clickable((By.XPATH, "(//a[contains(text(),'Add to cart')])[1]"))).click()
+    except ElementClickInterceptedException:
+        driver.execute_script("arguments[0].click();", add_button)
 
-    wait.until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(),'Shopping Cart') or contains(text(),'Cart')]")))
+    view_cart = wait.until(EC.element_to_be_clickable((By.XPATH, "//u[contains(text(),'View Cart')]")))
+    view_cart.click()
+
+    # Assert we are on the cart page
+    assert "Shopping Cart" in driver.page_source
